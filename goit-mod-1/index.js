056a25aa0e200ac219eff2c5d2c26783e6810f5a
Danilo Glushko <39314465+BigBlueDraco@users.noreply.github.com>
2022-11-29 15:40:49 +0200

    edit console log

--- goit-mod-1/index.js
+++ goit-mod-1/index.js
@@ -8,8 +8,6 @@ import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 const argv = yargs(hideBin(process.argv)).argv;
 
-console.log(argv);
-
 const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
     case "list":
